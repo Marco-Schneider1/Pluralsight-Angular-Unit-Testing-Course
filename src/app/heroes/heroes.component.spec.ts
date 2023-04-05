9e53f0f4db953d7741411702d2e55582c1ee15b7
Marco-Schneider1 <marco.a.rodrigues@exxonmobil.com>
2023-04-05 08:33:38 -0300

    Inclusion of more tests

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,15 @@
+import { HeroesComponent } from "./heroes.component";
+
+describe('HeroesComponent', () => {
+  let component: HeroesComponent;
+  let HEROES;
+
+  beforeEach(() => {
+    HEROES = [
+      {id: 1, name: "Hero1", strength: 8},
+      {id: 2, name: "Hero2", strength: 24},
+      {id: 3, name: "Hero3", strength: 55}
+    ]
+  })
+
+})
